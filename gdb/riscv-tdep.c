8e7fc3cc6117cefddd8a3c0407afc8dfc535d07f
Liviu Ionescu <ilg@livius.net>
2017-11-02 21:40:22 +0200

    riscv-tdep.c: s0 instead of fp name

--- gdb/riscv-tdep.c
+++ gdb/riscv-tdep.c
@@ -80,7 +80,7 @@ struct riscv_frame_cache
 static const char * const riscv_gdb_reg_names[RISCV_LAST_FP_REGNUM + 1] =
 {
   "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "fp", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6",
   "pc",
